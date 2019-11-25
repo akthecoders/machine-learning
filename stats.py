85b10488b55cd100ad4cebe1a794a469cdedbd7b
nxjiten <jiten@nodexperts.com>
2019-11-25 11:15:57 +0530

    Initial Commits

---
+++ stats.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 18 15:28:19 2019
+
+@author: akshay.kumar
+"""
+
+data = [1,2,3,3]
+
+import statistics as st
+mean = st.mean(data)
+mode = st.mode(data)
+std_dev = st.stdev(data)
+variance = st.variance(data)
+percentile = st
